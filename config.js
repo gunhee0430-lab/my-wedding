67c09912fc2a583c1bdbda757df55d8b0bf32e8d
gunhee0430-lab <gunhee0430@gmail.com>
2026-08-09 18:24:25 +0900

    Update config.js

--- config.js
+++ config.js
@@ -133,7 +133,7 @@ const CONFIG = {
     enabled: true,
     youtubeId: "BMA5fYUfXq0",     // 비워두면 음악 기능이 아예 표시되지 않음
     startAt: 1,        // 시작 지점(초). 전주가 길면 30 같은 값으로
-    volume: 20,        // 0~100
+    volume: 5,        // 0~100
     loop: true
   },
 
